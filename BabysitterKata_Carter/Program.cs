37cce586e056176cea2c3a43919d1e1edb0113c2
DannyElleCarter <DannyElleCarter@Outlook.com>
2019-05-01 00:40:08 -0400

    Initial commit "WriteLine for Family variable"

---
+++ BabysitterKata_Carter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace BabysitterKata_Carter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string inputStartTime = ""
+            
+        }
+        
+            while (true)
+        {
+            //User enters which family they are choosing
+            Console.WriteLine("Enter Family Name (A, B or C): ");
+        }
+
+        
+    }
+}
