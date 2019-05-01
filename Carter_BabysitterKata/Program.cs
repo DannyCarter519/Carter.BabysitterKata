26273ee0bb1b87324d2ba506dbd39587bf3d0dac
DannyElleCarter <DannyElleCarter@Outlook.com>
2019-05-01 02:02:08 -0400

    "added Family validation"

---
+++ Carter_BabysitterKata/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Carter_BabysitterKata
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Set app variables
+            string inputStartTime = "";
+            string family = "";
+
+            while (true)
+            {
+                //User enters which family they want
+                Console.WriteLine("Enter Family Name (A, B or C): ");
+
+                //Added"ToUpper to allow lowercase input too
+                family = Console.ReadLine().ToUpper();
+                
+                //Family choice validation
+                while (family != "A" && family != "B" && family != "C")
+                {
+                    Console.WriteLine("Please enter a valid family letter. Enter Family Name (A, B or C): ");
+                    family = Console.ReadLine();
+                }
+                
+                
+                
+
+            }
+
+        }
+    }
+}
